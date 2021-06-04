b4d90235968e112dea88d973ca6c6f1007aaf5f8
Cynthia Fu <yinnycfu@gmail.com>
2021-06-04 23:23:36 +0100

    created SecretDiary class

---
+++ lib/secretdiary.rb
@@ -0,0 +1,33 @@
+class SecretDiary
+  
+  def initialize
+    @locked = true
+  end
+
+  def add_entry(message)
+    raise 'It is locked!' if locked?
+
+    @message = message
+  end
+
+  def get_entries
+    raise 'It is locked!' if locked?
+
+    @message
+  end
+
+  def unlock
+    @locked = false
+  end
+
+  def lock
+    @locked = true
+  end
+
+  private
+
+  def locked?
+    @locked == true
+  end
+
+end
---
+++ spec/secretdiary_spec.rb
@@ -0,0 +1,46 @@
+require 'secretdiary'
+
+describe SecretDiary do
+  let(:message) { "I love McDonalds!" }
+
+  context 'initially locked' do
+    it 'raises an error when trying to add an entry' do
+      expect {subject.add_entry(message)}.to raise_error 'It is locked!'
+    end
+
+    it 'raises an error when trying to get entries' do
+      expect {subject.get_entries}.to raise_error 'It is locked!'
+    end
+  end
+
+  context 'unlocked' do
+    before do
+      subject.unlock
+    end
+
+    it 'can add an entry' do
+      expect(subject.add_entry(message)).to eq message
+    end
+
+    it 'can retrieve entries' do
+      subject.add_entry(message)
+
+      expect(subject.get_entries).to eq message
+    end
+
+    context 'can lock the diary again' do
+      before do
+        subject.lock
+      end
+
+      it 'raises an error when trying to add an entry' do
+        expect {subject.add_entry(message)}.to raise_error 'It is locked!'
+      end
+  
+      it 'raises an error when trying to get entries' do
+        expect {subject.get_entries}.to raise_error 'It is locked!'
+      end
+    end
+
+  end
+end
